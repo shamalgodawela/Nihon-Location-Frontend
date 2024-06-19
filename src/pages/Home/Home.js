fc74a1c33efbf48ed823222307365f443d47a5b5
shamal melantha <shamalmelantha3@gmail.com>
2024-06-19 12:25:55 +0530

    update homejs

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -19,7 +19,7 @@ const MyComponent = () => {
       </div> 
       <div className="center-container">
       <button type="button" className="btn btn-outline-success">
-          <Link className='link1' to="/login-admin">Admin Login</Link>
+          <Link className='link1' to="/getalllocation">Admin Login</Link>
       </button>
         
         
